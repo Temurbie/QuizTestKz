d30b0f5aa82ae1ca84a9efef3a991937204f3641
Temurbie <160334075+Temurbie@users.noreply.github.com>
2026-05-02 18:16:15 +0500

    Update authData.ts

--- src/app/data/authData.ts
+++ src/app/data/authData.ts
@@ -12,5 +12,24 @@ export const AUTHDATA: IAuth[] = [
         login : 'admin',
         parol: 'admin123'
     },
+   {
+        id: 2,
+        ban: false,
+        login : 'Jasmina7',
+        parol: 'jasmina45'
+    },
+    
+       {
+        id: 3,
+        ban: false,
+        login : 'Shermatova',
+        parol: '4736sh'
+    },
+       {
+        id: 4,
+        ban: false,
+        login : 'sunshine',
+        parol: 'sun198'
+    },
 
 ]
